200922ec74f8d27b0db020e2c85a2bbbc37b4451
Prangan <108721928+Spidy32@users.noreply.github.com>
2024-05-25 14:25:03 +0530

    Update Education.jsx

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -5,10 +5,7 @@ const Education = () => {
     <section className="light" id="education">
       <h2 className="edu">Education</h2>
       <br />
-      <h3>Bachelor of Computer APPLICATION (BCA)</h3>
-      <p>Maharshi Dayanand University Year of completion -2022</p>
-      <p>12th passed from CBSE Board</p>
-      <p>10th passed from CBSE Board</p>
+  
       <br />
       <a href="https://www.hackerrank.com/certificates/ee7101de9fe2">CSS Certificate</a>
       <br /> <br />
